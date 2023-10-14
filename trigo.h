4beaa5e4a14dd7d6e94f8b97c1640a1050f75935
19atlas <67108558+19atlas@users.noreply.github.com>
2023-10-14 23:54:08 +0300

    add bolgeler

--- trigo.h
+++ trigo.h
@@ -3,11 +3,25 @@
 char* trigo[] = {"","cos","sin","tan","cot"};
 const char* ffonc;
 
-void hangifonc(int a){
-    if(a >= 1 && a <= 4){
-        ffonc = trigo[a];
+void hangifonc(int s){
+    if(s >= 1 && s <= 4){
+        ffonc = trigo[s];
     } else {
         printf("Hatalı secim");
         exit(1);
     }
 }
+void bolgeal(int aci){
+    int abolge;
+    if (aci >= 0 && aci < 90) {
+        abolge = 1;
+    } else if (aci > 90 && aci < 180) {
+        abolge = 2;
+    } else if (aci > 90 && aci < 180) {
+        abolge = 3;
+    }else if (aci > 90 && aci < 180) {
+        abolge = 4;
+    }
+    printf("%d. bolgede",abolge);
+}
+//void isaret(){}
