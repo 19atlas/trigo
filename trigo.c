26b70913808795969d622e8fcdac6bb43205f439
19atlas <67108558+19atlas@users.noreply.github.com>
2023-10-19 21:12:13 +0300

    Update trigo.c

--- trigo.c
+++ trigo.c
@@ -5,25 +5,25 @@
 
 int secim;
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     printf("indirgeme uygulanacak fonk. sec\n1.cos\n2.sin\n3.tan\n4.cot\n==>");
     scanf("%d",&secim);
     hangifonc(secim);
     printf("%s(x)\n",ffonc);
-    if (strcmp (argv[1], "-d") == 0) {
-        int derece_d = 0;
-        printf("x bir derece olmak üzere,\nx = ");
-        scanf("%d",&derece_d);
-        islem_d(derece_d,secim);
+    for (int i = 1; i < argc; i++) {
+        if (strcmp (argv[i], "--derece") == 0) {
+            int derece_d = 0;
+            printf("x bir derece olmak üzere,\nx = ");
+            scanf("%d",&derece_d);
+            islem_d(derece_d,secim);
 
-    } else {
-        char radyan[10];
-        printf("fonksiyonun icindeki deger örn. 2/3+a\n? ");
-        scanf("%s",radyan);
+        } else {
+            char radyan[10];
+            printf("fonksiyonun icindeki deger örn. 2/3+a\n? ");
+            scanf("%s",radyan);
         //double radyan = aci * M_PI / 180; // Dereceden radyana dönüşüm
-        islem_r(radyan,secim);
-        
-        
+            islem_r(radyan,secim);
+        }
     }
     return 0;
     
