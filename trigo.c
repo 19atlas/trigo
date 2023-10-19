42a52ae6d6564d09f2444c0bf3a58b76e81f9177
19atlas <67108558+19atlas@users.noreply.github.com>
2023-10-19 16:57:08 +0300

    bug fix

--- trigo.c
+++ trigo.c
@@ -13,12 +13,18 @@ int main(int argc, char **argv) {
     if (strcmp (argv[1], "--derece") == 0) {
         int derece_d = 0;
         printf("x bir derece olmak üzere,\nx = ");
-        scanf("%d",derece_d);
+        scanf("%d",&derece_d);
+        islem_d(derece_d);
 
     } else {
-        printf("π nin kac kati?\nörn. 2/3\n= ");
+        char radyan[10];
+        printf("fonksiyonun icindeki deger örn. 2/3+a\n? ");
+        scanf("%s",radyan);
         //double radyan = aci * M_PI / 180; // Dereceden radyana dönüşüm
+        islem_r(radyan);
+        
         
     }
+    return 0;
     
 }
